71d1d0e78a12ecab1d1e3906f0bc2eb2b7366b76
robertxpearce <robertbdpearce@gmail.com>
2024-06-24 20:03:59 -0700

    Made comments for guide.

---
+++ projectileMotionSim.py
@@ -0,0 +1,28 @@
+# Robert Pearce
+# 06/24/2024
+
+import numpy as np
+
+# Initial Velocity Function
+def initialV(v, m):
+
+
+# Time of Flight Function
+
+
+
+# Max Height Function
+
+
+
+# Range Function
+
+
+# Main function
+def main():
+
+
+
+# Call main function.
+if __name__ == "__main__":
+    main()
