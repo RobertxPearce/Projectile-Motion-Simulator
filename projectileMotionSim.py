e8553d4698ed24a223dd788df8194f2e65af52bb
Robert <robertpearcetv@gmail.com>
2024-06-24 20:26:47 -0700

    Finished initial velocity and total time functions.

--- projectileMotionSim.py
+++ projectileMotionSim.py
@@ -4,10 +4,23 @@
 import numpy as np
 
 # Initial Velocity Function
-def initialV(v, m):
+# v : Initial speed of projectile.
+# m : Launch angle of projectile.
+def initialVelocity(v, theta):
+    # Convert angle to radians (numpy uses rad).
+    theta_rad = np.radians(theta)
+    # Calculate the initial horizontal velocity.
+    vX = v * np.cos(theta_rad) # v_x = v * cos(theta)
+    # Calculate the initial vertical velocity.
+    vY = v * np.sin(theta_rad) # v_y = v * sin(theta)
 
-
-# Time of Flight Function
+# Total Time of Whole Journey Function
+# vY : Vertical velocity.
+# g  : Gravity
+def totalTime(vY, g):
+    # time = (2 * v0 * sin(theta)) / |g|
+    T = (2 * vY) / g
+    return T
 
 
 
@@ -20,7 +33,8 @@ def initialV(v, m):
 
 # Main function
 def main():
-
+    # Gravity Constant
+    G = 9.81 # m/s^2
 
 
 # Call main function.
