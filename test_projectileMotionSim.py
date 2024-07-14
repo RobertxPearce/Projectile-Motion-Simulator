421d9c0767e5912df950c612e19c6686f1c04714
Robert <robertpearcetv@gmail.com>
2024-07-13 18:06:24 -0700

    Added unit test for initial velocity and max heigh functions.

--- test_projectileMotionSim.py
+++ test_projectileMotionSim.py
@@ -1,3 +1,61 @@
 # Robert Pearce
 # 06/24/2024
 
+import pytest
+from projectileMotionSim import initial_velocity
+from projectileMotionSim import total_time
+from projectileMotionSim import max_height
+from projectileMotionSim import total_range
+
+
+def test_initial_velocity():
+    """
+    Example problem for initial velocity: OpenStax, University Physics Volume 1, Chapter 4.3, Example 4.7
+    """
+    _initial_velocity = 70.0  # Initial speed provided by problem.
+    _launch_angle = 75.0      # Launch angle provided by problem.
+
+    # Find the vertical velocity.
+    vX, vY = initial_velocity(_initial_velocity, _launch_angle)
+
+    # Check if the horizontal velocity matches the answer provided in textbook.
+    assert pytest.approx(vX, 0.1) == 18.1
+
+    # Check if the vertical velocity matches the answer provided in textbook.
+    assert pytest.approx(vY, 0.1) == 67.6
+
+
+
+# def test_total_time():
+#     """
+#     Example problem for total time of flight: OpenStax, University Physics Volume 1, Chapter 4.3, Example 4.8
+#     """
+
+
+
+
+def test_max_height():
+    """
+    Example problem for maximum height: OpenStax, University Physics Volume 1, Chapter 4.3, Example 4.7
+    """
+    _initial_velocity = 70.0  # Initial speed provided by problem.
+    _launch_angle = 75.0      # Launch angle provided by problem.
+    g = 9.81                 # Gravity constant.
+
+    # Find the vertical velocity.
+    vX, vY = initial_velocity(_initial_velocity, _launch_angle)
+
+    # Round answer to whole number for assert.
+    _max_height = round(max_height(vY, g))
+
+    # Check if max height function return value matches answer given by textbook.
+    assert _max_height == 233
+
+
+# def test_total_range():
+#     """
+#     Example problem for total range: OpenStax, University Physics Volume 1, Chapter 3, Example 3.4
+#     """
+
+if __name__ == "__main__":
+    pytest.main()
