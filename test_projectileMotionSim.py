0a346ed1d7a12fdad3e4c391f5fb64746930d06c
Robert <robertpearcetv@gmail.com>
2024-07-13 18:30:52 -0700

    Added total range test.

--- test_projectileMotionSim.py
+++ test_projectileMotionSim.py
@@ -25,13 +25,18 @@ def test_initial_velocity():
     assert pytest.approx(vY, 0.1) == 67.6
 
 
+def test_total_time():
+    """
+    Example problem for total time of flight: OpenStax, University Physics Volume 1, Chapter 4.3, Example 4.8
+    """
+    vY = 21.2   # Vertical velocity provided by problem.
+    G = 9.81    # Gravity constant.
 
-# def test_total_time():
-#     """
-#     Example problem for total time of flight: OpenStax, University Physics Volume 1, Chapter 4.3, Example 4.8
-#     """
+    # Call function to compute total time.
+    _total_time = total_time(vY, G)
 
-
+    # Check if total time matches the answer provided in textbook.
+    assert pytest.approx(_total_time, 0.1) == 4.32
 
 
 def test_max_height():
@@ -40,13 +45,13 @@ def test_max_height():
     """
     _initial_velocity = 70.0  # Initial speed provided by problem.
     _launch_angle = 75.0      # Launch angle provided by problem.
-    g = 9.81                 # Gravity constant.
+    G = 9.81                  # Gravity constant.
 
     # Find the vertical velocity.
     vX, vY = initial_velocity(_initial_velocity, _launch_angle)
 
     # Round answer to whole number for assert.
-    _max_height = round(max_height(vY, g))
+    _max_height = round(max_height(vY, G))
 
     # Check if max height function return value matches answer given by textbook.
     assert _max_height == 233
@@ -54,8 +59,9 @@ def test_max_height():
 
 # def test_total_range():
 #     """
-#     Example problem for total range: OpenStax, University Physics Volume 1, Chapter 3, Example 3.4
+#     Example problem for total range:
 #     """
+
 
 if __name__ == "__main__":
     pytest.main()
